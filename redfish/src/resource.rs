cd21e1d3e3793729e839fc44b7555b33676a7004
Dmitry Porokh <dporokh@nvidia.com>
2025-11-17 09:44:21 -0800

    nv-redfish: use TaggedType for ResourceId and ResourceDescription

--- redfish/src/resource.rs
+++ redfish/src/resource.rs
@@ -16,6 +16,29 @@
 //! Redfish resource
 
 use crate::ResourceSchema;
+use tagged_types::TaggedType;
+
+/// Redfish resource identifier.
+pub type ResourceId = TaggedType<String, ResourceIdTag>;
+/// Reference to Redfish resource identifier.
+pub type ResourceIdRef<'a> = TaggedType<&'a String, ResourceIdTag>;
+#[doc(hidden)]
+#[derive(tagged_types::Tag)]
+#[implement(Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[transparent(Debug, Display, FromStr)]
+#[capability(inner_access, cloned)]
+pub enum ResourceIdTag {}
+
+/// Redfish resource description.
+pub type ResourceDescription = TaggedType<String, ResourceDescriptionTag>;
+/// Reference to Redfish resource description.
+pub type ResourceDescriptionRef<'a> = TaggedType<&'a String, ResourceDescriptionTag>;
+#[doc(hidden)]
+#[derive(tagged_types::Tag)]
+#[implement(Clone)]
+#[transparent(Debug, Display, FromStr)]
+#[capability(inner_access, cloned)]
+pub enum ResourceDescriptionTag {}
 
 /// Represents Redfish Resource base type.
 pub trait Resource {
@@ -23,15 +46,16 @@ pub trait Resource {
     fn resource_ref(&self) -> &ResourceSchema;
 
     /// Identifier of the resource.
-    fn id(&self) -> &String {
-        &self.resource_ref().id
+    fn id(&self) -> ResourceIdRef<'_> {
+        ResourceIdRef::new(&self.resource_ref().id)
     }
 
     /// Description of the resource.
-    fn description(&self) -> Option<&String> {
+    fn description(&self) -> Option<ResourceDescriptionRef<'_>> {
         self.resource_ref()
             .description
             .as_ref()
             .and_then(|v| v.as_ref())
+            .map(ResourceDescriptionRef::new)
     }
 }
